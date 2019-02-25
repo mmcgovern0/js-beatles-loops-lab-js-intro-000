9dc1481bfdcb130ede9d9306fcd1ef2b286e24bb
Michael McGovern <mmcgovern.wework@gmail.com>
2019-02-25 14:13:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+
+  for(let i=0, 1 = musicians.length; i < 1; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
